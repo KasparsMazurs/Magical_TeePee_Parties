7e2b518f1b6480a0045f5828dba20c1f6f075ad5
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-03-11 18:05:23 +0000

    building cobuild section to leave a comment

--- TeePee/settings.py
+++ TeePee/settings.py
@@ -43,9 +43,17 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'django_summernote',
     'blog',
 ]
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
+from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
@@ -22,5 +23,31 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+            },
+        )
+    
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.filter(status=1)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
             },
         )
