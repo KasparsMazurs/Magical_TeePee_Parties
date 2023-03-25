b3cfe3892c906c1190735d9297ddb070e6b7f0fc
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-03-25 13:47:09 +0000

    change navigation bar

---
+++ emv.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://buhouxwf:7v4wDWgKnlmYGz_25V0RJqLOWrs3IPd6@trumpet.db.elephantsql.com/buhouxwf"
+os.environ["SECRET_KEY"] = "Magical_TeePee_Parties"
+os.environ["CLOUDINARY_URL"] = "cloudinary://599952223867666:p-yP43tz2wiuoNuEgBdYiOA1NiU@dushhrb6z"
