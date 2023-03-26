1f785ead025486734e0e39b782d2f5c80df6b712
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-03-26 13:08:05 +0000

    Make slight adjustments in urls.py to open product page

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,13 @@
 from . import views
 from django.urls import path
-from .views import GalleryListView, SeeGalleryView, ProductsListView
+from .views import GalleryListView, SeeGalleryView, ProductsListView, ProductView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('gallery/', GalleryListView.as_view(), name='gallery'),
     path('gallery/<slug:slug>/', SeeGalleryView.as_view(), name='see_gallery'),
     path('products/', ProductsListView.as_view(), name='products'),
-    path('products/<slug:slug>/', ProductsListView.as_view(), name='products_detail'),
+    path('products/<slug:slug>/', ProductView.as_view(), name='products_detail'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
