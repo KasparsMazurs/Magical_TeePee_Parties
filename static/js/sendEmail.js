77bb648481eaf7aa1b98a8bfd5bd2bf278dac229
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-04-01 11:28:06 +0000

    create SUCCESS masege when email was sent SUCCESSFUL

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -7,9 +7,12 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            $('.alert').show();
         },
         function(error) {
             console.log("FAILED", error);
-        }
-    );
-}
+    });
+    
+    return false; // Prevent the default form submission behavior
+    
+    }
