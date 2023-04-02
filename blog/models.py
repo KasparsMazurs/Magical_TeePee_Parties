1add762f8523f16dd47adfef918f5c74637bb46f
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-04-02 18:53:13 +0000

    add to models additional_info for bookings and style booking form

--- blog/models.py
+++ blog/models.py
@@ -129,6 +129,7 @@ class BookAParty(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     approved = models.BooleanField(default=False)
     email = models.EmailField(default='', blank=False)
+    additional_info = models.TextField(default='You can add some additional information or ask us a question here.')
     order_nr = models.SlugField(max_length=200, unique=True, editable=False, default='new_order')
 
     def save(self, *args, **kwargs):
