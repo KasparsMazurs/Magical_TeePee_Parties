d694da0444e4668b82633bda178474e9fa9d176a
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-04-15 10:41:58 +0000

    Add comments end fix PEP8 errors in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
-from .views import GalleryListView, SeeGalleryView, ProductsListView, ProductView, ContactUsView, AboutUsView, BookAPartyView, submitted_parties, EditPartyView
+from .views import (GalleryListView, SeeGalleryView, ProductsListView,
+                    ProductView, ContactUsView, AboutUsView, BookAPartyView,
+                    submitted_parties, EditPartyView, )
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
@@ -9,10 +11,12 @@ urlpatterns = [
     path('gallery/', GalleryListView.as_view(), name='gallery'),
     path('gallery/<slug:slug>/', SeeGalleryView.as_view(), name='see_gallery'),
     path('products/', ProductsListView.as_view(), name='products'),
-    path('products/<slug:slug>/', ProductView.as_view(), name='products_detail'),
+    path('products/<slug:slug>/', ProductView.as_view(),
+         name='products_detail'),
     path('bookapaty/', BookAPartyView.as_view(), name='book_a_paty'),
     path('submitted-parties/', submitted_parties, name='submitted_parties'),
-    path('submitted-parties/<slug:order_nr>/edit/', EditPartyView.as_view(), name='edit_party'),
+    path('submitted-parties/<slug:order_nr>/edit/', EditPartyView.as_view(),
+         name='edit_party'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
