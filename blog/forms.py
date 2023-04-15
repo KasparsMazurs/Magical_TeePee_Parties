00ddded7002c259c51cc4c809b291e52d88c2c64
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-04-15 10:03:01 +0000

    Add comments end fix PEP8 errors in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,12 +3,20 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Create a form to make comments
+    """
     class Meta:
         model = Comment
         fields = ('body',)
 
 
 class BookingForm(forms.ModelForm):
+    """
+    Create a form to book a party
+    """
     class Meta:
         model = BookAParty
-        fields = ('party_theme', 'balloons', 'bouncy_castle', 'kids_age', 'number_of_teepees', 'street', 'city', 'county', 'eircode', 'date', 'email', 'phone_number', 'additional_info',)
+        fields = ('party_theme', 'balloons', 'bouncy_castle', 'kids_age',
+                  'number_of_teepees', 'street', 'city', 'county', 'eircode',
+                  'date', 'email', 'phone_number', 'additional_info',)
