6a94cd171e863b434ecfc3f98222de19b02395db
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-04-23 08:44:45 +0000

    change x-frame settings

--- TeePee/settings.py
+++ TeePee/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 ALLOWED_HOSTS = ["magical-teepee-parties.herokuapp.com", "localhost",
                  "127.0.0.1:8000", "127.0.0.1"]
